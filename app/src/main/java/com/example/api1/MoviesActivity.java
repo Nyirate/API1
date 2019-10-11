3033663a1f49b26ee64a2abd9d84e6b405daed30
Nyirate <rwakaemma34@gmail.com>
2019-10-12 01:08:34 +0200

    add adapter

--- app/src/main/java/com/example/api1/MoviesActivity.java
+++ app/src/main/java/com/example/api1/MoviesActivity.java
@@ -2,7 +2,9 @@ package com.example.api1;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,6 +17,9 @@ public class MoviesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movies);
         mListView = (ListView) findViewById(R.id.listView);
+        mGoTextView = (TextView) findViewById(R.id.goTextView);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, types);
+        mListView.setAdapter(adapter);
 
     }
 }
