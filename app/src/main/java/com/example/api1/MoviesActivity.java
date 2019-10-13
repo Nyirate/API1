0ceae46a025598e6b1706b589014cebc040489ae
Nyirate <rwakaemma34@gmail.com>
2019-10-13 15:26:26 +0200

    add grid view

--- app/src/main/java/com/example/api1/MoviesActivity.java
+++ app/src/main/java/com/example/api1/MoviesActivity.java
@@ -7,35 +7,31 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
 import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MoviesActivity extends AppCompatActivity {
-    private ListView mView1;
-    GridView gridView;
-    private int [] images = {R
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
+public class MoviesActivity extends AppCompatActivity {
+    @BindView(R.id.grid)
+    GridView gridView;
+    private int [] images = {R.drawable. action,
+                             R.drawable. comedy,
+                             R.drawable. thriller,
+                             R.drawable. horror,
+                             R.drawable. christian,
+                             R.drawable. romance
     };
-    private String[] types = new String[]{"Action Film", "Comedy Film", "Horror Film", "Romance Film", "Thriller Film", "Christian Film"};
+    private String[] types = new String[]{"Action", "Comedy ", "Thriller", "Horror", "Christian", "Romance"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movies);
-        MoviesActivityArrayAdapter adapter = new MoviesActivityArrayAdapter(this, android.R.layout.simple_list_item_1, types);
-        mView1.setAdapter(adapter);
-        mView1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        ButterKnife.bind(this);
+        gridView=(GridView)findViewById(R.id.grid);
 
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                String type = ((TextView)view).getText().toString();
 
-            }
-        });
-
-        Intent intent = getIntent();
 
 
 
--- app/src/main/java/com/example/api1/MoviesActivityArrayAdapter.java
+++ app/src/main/java/com/example/api1/MoviesActivityArrayAdapter.java
@@ -1,29 +1,51 @@
 package com.example.api1;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 
-public class MoviesActivityArrayAdapter extends ArrayAdapter {
+public class MoviesActivityArrayAdapter extends BaseAdapter {
     private Context mContext;
     private String[] mTypes;
-    //    private String[] mAuthors;
-    public MoviesActivityArrayAdapter(Context mContext, int resource, String[] mTypes) {
-        super(mContext, resource);
+    private  int[] images;
+
+
+    public MoviesActivityArrayAdapter(Context mContext, String[] mTypes, int[] images) {
         this.mContext = mContext;
         this.mTypes = mTypes;
-//        this.mAuthors = mAuthors;
-    }
-    @Override
-    public Object getItem(int position) {
-        String type = mTypes[position];
-//        String author = mAuthors[position];
-        return String.format( type);
+        this.images = images;
     }
 
     @Override
     public int getCount() {
         return mTypes.length;
     }
+
+    @Override
+    public Object getItem(int position) {
+        return mTypes[position];
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View emma = convertView;
+        if (emma == null) {
+            LayoutInflater val = (LayoutInflater).mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            emma=val.inflate(R.layout.activity_movies,null);
+        }
+
+        return null;
+    }
+    //    private String[] mAuthors;
+
 }
 
 
