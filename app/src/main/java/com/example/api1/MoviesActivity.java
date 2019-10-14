1114455d956869740e746ec2de2c2683f13328d1
Nyirate <rwakaemma34@gmail.com>
2019-10-14 10:40:14 +0200

    change app color

--- app/src/main/java/com/example/api1/MoviesActivity.java
+++ app/src/main/java/com/example/api1/MoviesActivity.java
@@ -21,6 +21,8 @@ import butterknife.ButterKnife;
 public class MoviesActivity extends AppCompatActivity {
     private String[] types = new String[]{"Action", "Comedy ", "Thriller", "Horror", "Christian", "Romance"};
     private TextView mTel;
+    private TextView mTel2;
+    private TextView mTel5;
     private int [] images = {R.drawable. action,
                              R.drawable. comedy,
                              R.drawable. thriller,
@@ -38,6 +40,8 @@ public class MoviesActivity extends AppCompatActivity {
         setContentView(R.layout.activity_movies);
 
         mTel = (TextView) findViewById(R.id.tel);
+        mTel2 = (TextView) findViewById(R.id.tel2);
+        mTel5 = (TextView) findViewById(R.id.tel5);
 
 
         mTel.setOnClickListener(new  View.OnClickListener(){
@@ -54,8 +58,35 @@ public class MoviesActivity extends AppCompatActivity {
 
             }
         });
+        mTel2.setOnClickListener(new  View.OnClickListener(){
 
 
+            @Override
+            public void onClick(View view) {
+                Intent intent1 = getIntent();
+                Intent intent = new Intent(MoviesActivity.this, ThrillerActivity.class);
+
+
+                startActivity(intent);
+
+
+            }
+        });
+
+        mTel5.setOnClickListener(new  View.OnClickListener(){
+
+
+            @Override
+            public void onClick(View view) {
+                Intent intent1 = getIntent();
+                Intent intent = new Intent(MoviesActivity.this, RomanceActivity.class);
+
+
+                startActivity(intent);
+
+
+            }
+        });
 
 
     }
